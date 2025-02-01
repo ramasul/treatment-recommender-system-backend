b2956ef3eea3ebb6a5c08d884a1385bae42d40f4
ramasul <ramasulaimannurcahyo@mail.ugm.ac.id>
2025-02-01 22:10:04 +0700

    feat: embedding and graph documents utils

--- src/shared/utils.py
+++ src/shared/utils.py
@@ -2,8 +2,10 @@ import os
 import re
 import logging
 from typing import List
+from pathlib import Path
 from urllib.parse import urlparse
 from langchain_neo4j import Neo4jGraph
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.graphs.graph_document import GraphDocument
 
 #Fungsi yang digunakan secara umum
@@ -58,3 +60,55 @@ def close_db_connection(graph, api_name):
   if not graph._driver._closed:
       logging.info(f"closing connection for {api_name} api")
       graph._driver.close()   
+
+
+def get_chunk_and_graphDocument(graph_document_list, chunkId_chunkDoc_list):
+  logging.info("Creating list of chunks and graph documents in get_chunk_and_graphDocument func")
+  lst_chunk_chunkId_document=[]
+  for graph_document in graph_document_list:            
+          for chunk_id in graph_document.source.metadata['combined_chunk_ids'] :
+            lst_chunk_chunkId_document.append({'graph_doc':graph_document,'chunk_id':chunk_id})
+                  
+  return lst_chunk_chunkId_document  
+
+def load_embedding_model(embedding_model_name: str):
+    if embedding_model_name == "huggingface":
+        embeddings = HuggingFaceEmbeddings(
+            model_name="all-MiniLM-L6-v2"#, cache_folder="/embedding_model"
+        )
+        dimension = 384
+        logging.info(f"Embedding: Using Langchain HuggingFaceEmbeddings , Dimension:{dimension}")
+    else:
+        err = f"Embedding model {embedding_model_name} is not supported"
+        logging.error(err)
+        raise Exception(err)
+    return embeddings, dimension
+
+def handle_backticks_nodes_relationship_id_type(graph_document_list:List[GraphDocument]):
+    for graph_document in graph_document_list:
+      # Clean node id and types 
+      cleaned_nodes = []
+      for node in graph_document.nodes:
+         if node.type.strip() and node.id.strip():
+            node.type = node.type.replace('`', '')
+            cleaned_nodes.append(node)
+
+      # Clean relationship id types and source/target node id and types
+      cleaned_relationships = []
+      for rel in graph_document.relationships:
+         if rel.type.strip() and rel.source.id.strip() and rel.source.type.strip() and rel.target.id.strip() and rel.target.type.strip():
+            rel.type = rel.type.replace('`', '')
+            rel.source.type = rel.source.type.replace('`', '')
+            rel.target.type = rel.target.type.replace('`', '')
+            cleaned_relationships.append(rel)
+      graph_document.relationships = cleaned_relationships
+      graph_document.nodes = cleaned_nodes
+    return graph_document_list
+
+def delete_uploaded_local_file(merged_file_path, file_name):
+  file_path = Path(merged_file_path)
+  if file_path.exists():
+    file_path.unlink()
+    logging.info(f'file {file_name} deleted successfully')
+
+
